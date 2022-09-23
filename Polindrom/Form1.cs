372951733a2a928acb0777c47c1c2f1c5d3420eb
TheLonestar1 <yura2000077@yandex.ru>
2022-09-24 04:35:00 +0700

    Добавьте файлы проекта.

---
+++ Polindrom/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Polindrom
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+
+
+        void Check()
+        {
+            string text = textBox1.Text;
+            char[] obrtext = text.ToArray();
+            Array.Reverse(obrtext);
+            string final = new string(obrtext);
+            if (text == final)
+                label1.Text = "Это полиндром";
+            else
+                label1.Text = "Это не полиндром" + final;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Check();
+        }
+    }
+}
